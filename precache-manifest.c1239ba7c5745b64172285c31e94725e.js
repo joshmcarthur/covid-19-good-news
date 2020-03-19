464bd4cf25e185336b83d1db190cc82468ef2397
Josh McArthur <joshua.mcarthur@gmail.com>
2020-03-20 12:23:14 +1300

    Updates

---
+++ precache-manifest.c1239ba7c5745b64172285c31e94725e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7593fb70bb7b658ea6501df64236c2",
+    "url": "/covid-19-good-news/index.html"
+  },
+  {
+    "revision": "f80aeffc76f8110b6365",
+    "url": "/covid-19-good-news/static/css/main.87156af7.chunk.css"
+  },
+  {
+    "revision": "45a1e34a15a4da09ce64",
+    "url": "/covid-19-good-news/static/js/2.88386ab8.chunk.js"
+  },
+  {
+    "revision": "9cb8761efa01e947c5e7c24276931488",
+    "url": "/covid-19-good-news/static/js/2.88386ab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f80aeffc76f8110b6365",
+    "url": "/covid-19-good-news/static/js/main.3b98548e.chunk.js"
+  },
+  {
+    "revision": "3d98fb021284d1d58c87",
+    "url": "/covid-19-good-news/static/js/runtime-main.ca30cf16.js"
+  }
+]);
